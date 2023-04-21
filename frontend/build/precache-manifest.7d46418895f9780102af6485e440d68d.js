7b66d801dea9a01bed333cccaf15b08278b9c083
dubey748 <dubeyabhishek748@gmail.com>
2023-04-22 03:16:04 +0530

    first commit

---
+++ frontend/build/precache-manifest.7d46418895f9780102af6485e440d68d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf4b7791b94042841282",
+    "url": "/static/css/main.e2ddee37.chunk.css"
+  },
+  {
+    "revision": "cf4b7791b94042841282",
+    "url": "/static/js/main.cf4b7791.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "c2e3b4d0a96aa994424d",
+    "url": "/static/css/2.01cf48e4.chunk.css"
+  },
+  {
+    "revision": "c2e3b4d0a96aa994424d",
+    "url": "/static/js/2.c2e3b4d0.chunk.js"
+  },
+  {
+    "revision": "cf32845bfca8babfe8b0",
+    "url": "/static/js/runtime~main.cf32845b.js"
+  },
+  {
+    "revision": "ecc57c6490ff108032c3ee8e3be232eb",
+    "url": "/index.html"
+  }
+];
