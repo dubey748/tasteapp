abf8631ca6f71878ba6bf04f5002b79e65829dfc
dubey748 <dubeyabhishek748@gmail.com>
2023-07-17 22:22:08 +0530

    Fixed Toggeled

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Modal from "../Modal";
 import Cart from "../screens/Cart";
 import { useCart } from "./ContextReducer";
-
 import Badge from "react-bootstrap/Badge";
 
 const Navbar = () => {
   const [cartView, setCartView] = useState(false);
   const data = useCart();
-
   const navigate = useNavigate();
+  const navbarRef = useRef(null);
+  const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem("authToken");
     navigate("/login");
   };
+
+  const handleNavbarToggle = () => {
+    setIsNavbarExpanded(!isNavbarExpanded);
+  };
+
+  useEffect(() => {
+    const navbar = navbarRef.current;
+    const handleClickOutside = (event) => {
+      if (navbar && !navbar.contains(event.target)) {
+        setIsNavbarExpanded(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
@@ -25,26 +44,27 @@ const Navbar = () => {
             Taste
           </Link>
           <button
-            className="navbar-toggler"
+            className={`navbar-toggler ${isNavbarExpanded ? "collapsed" : ""}`}
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
             aria-controls="navbarNav"
-            aria-expanded="false"
+            aria-expanded={isNavbarExpanded}
             aria-label="Toggle navigation"
+            onClick={handleNavbarToggle}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`collapse navbar-collapse ${isNavbarExpanded ? "show" : ""}`} id="navbarNav" ref={navbarRef}>
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">
+                <Link className="nav-link active fs-5" aria-current="page" to="/" onClick={handleNavbarToggle}>
                   Home
                 </Link>
               </li>
               {localStorage.getItem("authToken") && (
                 <li className="nav-item">
-                  <Link className="nav-link active fs-5" aria-current="page" to="/MyOrder">
+                  <Link className="nav-link active fs-5" aria-current="page" to="/MyOrder" onClick={handleNavbarToggle}>
                     Orders
                   </Link>
                 </li>
@@ -52,11 +72,11 @@ const Navbar = () => {
             </ul>
             {!localStorage.getItem("authToken") ? (
               <div className="d-flex">
-                <Link className="btn bg-danger text-white m-1" to="/login">
+                <Link className="btn bg-danger text-white m-1" to="/login" onClick={handleNavbarToggle}>
                   Login
                 </Link>
 
-                <Link className="btn bg-danger text-white m-1" to="/createuser">
+                <Link className="btn bg-danger text-white m-1" to="/createuser" onClick={handleNavbarToggle}>
                   Sign Up
                 </Link>
               </div>
