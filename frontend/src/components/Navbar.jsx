aaaf52f90e01d4d1ad87150f5d6984db2ce77aaf
dubey748 <dubeyabhishek748@gmail.com>
2023-07-18 03:15:46 +0530

    Fixed Toggeled

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Modal from "../Modal";
 import Cart from "../screens/Cart";
@@ -6,14 +6,34 @@ import { useCart } from "./ContextReducer";
 import Badge from "react-bootstrap/Badge";
 
 const Navbar = () => {
-  const [cartView, setCartView] = React.useState(false);
+  const [cartView, setCartView] = useState(false);
   const data = useCart();
   const navigate = useNavigate();
+  const navbarRef = useRef(null);
+  const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem("authToken");
     navigate("/login");
   };
+
+  const handleNavbarToggle = () => {
+    setIsNavbarExpanded((prevExpanded) => !prevExpanded);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+      setIsNavbarExpanded(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   const handleCartClick = () => {
     setCartView(true);
@@ -31,26 +51,41 @@ const Navbar = () => {
             Taste
           </Link>
           <button
-            className="navbar-toggler"
+            className={`navbar-toggler ${isNavbarExpanded ? "" : "collapsed"}`}
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
             aria-controls="navbarNav"
-            aria-expanded="false"
+            aria-expanded={isNavbarExpanded}
             aria-label="Toggle navigation"
+            onClick={handleNavbarToggle}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div
+            className={`collapse navbar-collapse ${isNavbarExpanded ? "show" : ""}`}
+            id="navbarNav"
+            ref={navbarRef}
+          >
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                  onClick={() => setIsNavbarExpanded(false)}
+                >
                   Home
                 </Link>
               </li>
               {localStorage.getItem("authToken") && (
                 <li className="nav-item">
-                  <Link className="nav-link active fs-5" aria-current="page" to="/MyOrder">
+                  <Link
+                    className="nav-link active fs-5"
+                    aria-current="page"
+                    to="/MyOrder"
+                    onClick={() => setIsNavbarExpanded(false)}
+                  >
                     Orders
                   </Link>
                 </li>
@@ -58,10 +93,11 @@ const Navbar = () => {
             </ul>
             {!localStorage.getItem("authToken") ? (
               <div className="d-flex">
-                <Link className="btn bg-danger text-white m-1" to="/login">
+                <Link className="btn bg-danger text-white m-1" to="/login" onClick={() => setIsNavbarExpanded(false)}>
                   Login
                 </Link>
-                <Link className="btn bg-danger text-white m-1" to="/createuser">
+
+                <Link className="btn bg-danger text-white m-1" to="/createuser" onClick={() => setIsNavbarExpanded(false)}>
                   Sign Up
                 </Link>
               </div>
@@ -78,6 +114,7 @@ const Navbar = () => {
                     <Cart />
                   </Modal>
                 )}
+
                 <div className="btn bg-danger text-white m-1" onClick={handleLogout}>
                   Log out
                 </div>
