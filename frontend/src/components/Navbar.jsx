f080df719d5d6b283ef6d09a93d70f8d027cffce
dubey748 <dubeyabhishek748@gmail.com>
2023-07-18 03:21:43 +0530

    Fixed Toggeled

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Modal from "../Modal";
 import Cart from "../screens/Cart";
@@ -10,7 +10,7 @@ const Navbar = () => {
   const data = useCart();
   const navigate = useNavigate();
   const navbarRef = useRef(null);
-  const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem("authToken");
@@ -18,74 +18,60 @@ const Navbar = () => {
   };
 
   const handleNavbarToggle = () => {
-    setIsNavbarExpanded((prevExpanded) => !prevExpanded);
-  };
-
-  const handleOutsideClick = (event) => {
-    if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-      setIsNavbarExpanded(false);
-    }
+    setIsNavbarOpen((prevOpen) => !prevOpen);
   };
 
   useEffect(() => {
-    document.addEventListener("click", handleOutsideClick);
+    const handleClickOutside = (event) => {
+      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+        setIsNavbarOpen(false);
+      }
+    };
+
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape") {
+        setIsNavbarOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscapeKey);
 
     return () => {
-      document.removeEventListener("click", handleOutsideClick);
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
     };
   }, []);
-
-  const handleCartClick = () => {
-    setCartView(true);
-  };
-
-  const handleCartClose = () => {
-    setCartView(false);
-  };
 
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+        <div className="container-fluid" ref={navbarRef}>
           <Link className="navbar-brand fs-1 fst-italic" to="/">
             Taste
           </Link>
           <button
-            className={`navbar-toggler ${isNavbarExpanded ? "" : "collapsed"}`}
+            className={`navbar-toggler ${isNavbarOpen ? "" : "collapsed"}`}
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
             aria-controls="navbarNav"
-            aria-expanded={isNavbarExpanded}
+            aria-expanded={isNavbarOpen}
             aria-label="Toggle navigation"
             onClick={handleNavbarToggle}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div
-            className={`collapse navbar-collapse ${isNavbarExpanded ? "show" : ""}`}
-            id="navbarNav"
-            ref={navbarRef}
-          >
+          <div className={`collapse navbar-collapse ${isNavbarOpen ? "show" : ""}`} id="navbarNav">
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
-                <Link
-                  className="nav-link active fs-5"
-                  aria-current="page"
-                  to="/"
-                  onClick={() => setIsNavbarExpanded(false)}
-                >
+                <Link className="nav-link active fs-5" aria-current="page" to="/" onClick={handleNavbarToggle}>
                   Home
                 </Link>
               </li>
               {localStorage.getItem("authToken") && (
                 <li className="nav-item">
-                  <Link
-                    className="nav-link active fs-5"
-                    aria-current="page"
-                    to="/MyOrder"
-                    onClick={() => setIsNavbarExpanded(false)}
-                  >
+                  <Link className="nav-link active fs-5" aria-current="page" to="/MyOrder" onClick={handleNavbarToggle}>
                     Orders
                   </Link>
                 </li>
@@ -93,24 +79,24 @@ const Navbar = () => {
             </ul>
             {!localStorage.getItem("authToken") ? (
               <div className="d-flex">
-                <Link className="btn bg-danger text-white m-1" to="/login" onClick={() => setIsNavbarExpanded(false)}>
+                <Link className="btn bg-danger text-white m-1" to="/login" onClick={handleNavbarToggle}>
                   Login
                 </Link>
 
-                <Link className="btn bg-danger text-white m-1" to="/createuser" onClick={() => setIsNavbarExpanded(false)}>
+                <Link className="btn bg-danger text-white m-1" to="/createuser" onClick={handleNavbarToggle}>
                   Sign Up
                 </Link>
               </div>
             ) : (
               <div>
-                <div className="btn bg-danger text-white m-1" onClick={handleCartClick}>
+                <div className="btn bg-danger text-white m-1" onClick={() => setCartView(true)}>
                   Cart{" "}
                   <Badge pill bg="white" text="black">
                     {data.length}
                   </Badge>
                 </div>
                 {cartView && (
-                  <Modal onClose={handleCartClose}>
+                  <Modal onClose={() => setCartView(false)}>
                     <Cart />
                   </Modal>
                 )}
